730e2c620ee86b3fd7433323bcbc69c57eec53a7
Jordan McKinney <jordanmmck@gmail.com>
2023-07-11 21:22:49 -0700

    done

--- src/Escrow.sol
+++ src/Escrow.sol
@@ -1,14 +1,53 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Escrow {
-    uint256 public number;
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Escrow {
+    struct Deposit {
+        uint256 amount;
+        uint256 block;
     }
 
-    function increment() public {
-        number++;
+    // tokenAddress => depositer => recipient => (amount, timestamp)
+    mapping(address => mapping(address => mapping(address => Deposit))) public deposits;
+
+    function depositTokens(address tokenAddress, address recipient, uint256 amount) external {
+        // is this check too gas intensive to be worth while?
+        uint256 allowance = IERC20(tokenAddress).allowance(msg.sender, address(this));
+        require(allowance >= amount, "Insufficient allowance");
+
+        // transfer tokens to this contract
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
+        Deposit memory d = deposits[tokenAddress][msg.sender][recipient];
+
+        if (d.block == 0) {
+            // if this is the first deposit then set initial deposit struct
+            deposits[tokenAddress][msg.sender][recipient] = Deposit(amount, block.number);
+        } else {
+            // otherwise update existing deposit
+            deposits[tokenAddress][msg.sender][recipient].amount += amount;
+            deposits[tokenAddress][msg.sender][recipient].block = block.number;
+        }
+    }
+
+    function withdrawTokens(address tokenAddress, address depositer, uint256 amount) external {
+        Deposit memory d = deposits[tokenAddress][depositer][msg.sender];
+
+        require(block.number - d.block >= 21600, "Must wait 3 days");
+        require(amount <= d.amount, "Insufficient funds");
+        // worth the gas to check this? or simply attempt to transfer?
+        require(IERC20(tokenAddress).balanceOf(address(this)) >= amount, "Insufficient balance");
+        // adjust deposit to reflect funds taken out
+        deposits[tokenAddress][depositer][msg.sender].amount -= amount;
+
+        if (deposits[tokenAddress][depositer][msg.sender].amount == 0) {
+            // if no funds left then remove deposit
+            delete deposits[tokenAddress][depositer][msg.sender];
+        }
+
+        // call the token contract and transfer tokens to the withdrawer
+        // calling this last to prevent re-entrancy
+        IERC20(tokenAddress).transfer(msg.sender, amount);
     }
 }
---
+++ test/Escrow.t.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Escrow.sol";
+import "./mocks/MockERC20.sol";
+
+contract EscrowTest is Test {
+    Escrow public escrow;
+    MockERC20 public mockToken;
+
+    address public alice = address(0x1);
+    address public bob = address(0x2);
+    address public charlie = address(0x3);
+
+    function setUp() public {
+        escrow = new Escrow();
+        mockToken = new MockERC20();
+    }
+
+    function testDepositTokens() public {
+        mockToken.mint(alice, 10000e18);
+        vm.prank(alice);
+        mockToken.approve(address(escrow), 1e18);
+
+        vm.prank(alice);
+        escrow.depositTokens(address(mockToken), bob, 1e18);
+        assertEq(mockToken.balanceOf(address(escrow)), 1e18);
+
+        (uint256 amount, uint256 blockNum) = escrow.deposits(address(mockToken), alice, bob);
+        assertEq(amount, 1e18);
+        assertEq(blockNum, 1);
+    }
+
+    function testWithdrawTokensSuccess() public {
+        mockToken.mint(alice, 10000e18);
+        assertEq(mockToken.balanceOf(address(alice)), 10000e18);
+        vm.prank(alice);
+        mockToken.approve(address(escrow), 1e18);
+
+        vm.roll(1);
+        vm.prank(alice);
+        escrow.depositTokens(address(mockToken), bob, 1e18);
+        assertEq(mockToken.balanceOf(address(alice)), 9999e18);
+        assertEq(mockToken.balanceOf(address(bob)), 0);
+
+        vm.roll(1 + 21600);
+        vm.prank(bob);
+        escrow.withdrawTokens(address(mockToken), alice, 1e18);
+        assertEq(mockToken.balanceOf(address(bob)), 1e18);
+
+        // check that entry has been deleted
+        (uint256 amount, uint256 blockNum) = escrow.deposits(address(mockToken), alice, bob);
+        assertEq(amount, 0);
+        assertEq(blockNum, 0);
+    }
+
+    function testWithdrawTokensMustWait() public {
+        mockToken.mint(alice, 10000e18);
+        vm.prank(alice);
+        mockToken.approve(address(escrow), 1e18);
+
+        vm.roll(1);
+        vm.prank(alice);
+        escrow.depositTokens(address(mockToken), bob, 1e18);
+
+        vm.roll(1 + 21600 - 1);
+        vm.expectRevert();
+        vm.prank(bob);
+        escrow.withdrawTokens(address(mockToken), alice, 1e18);
+    }
+
+    function testPartialWithdraw() public {
+        mockToken.mint(alice, 10000e18);
+        vm.prank(alice);
+        mockToken.approve(address(escrow), 1e18);
+
+        vm.roll(1);
+        vm.prank(alice);
+        escrow.depositTokens(address(mockToken), bob, 1e18);
+
+        vm.roll(1 + 21600);
+        vm.prank(bob);
+        escrow.withdrawTokens(address(mockToken), alice, 0.5e18);
+
+        // check that entry has been deleted
+        (uint256 amount, uint256 blockNum) = escrow.deposits(address(mockToken), alice, bob);
+        assertEq(amount, 0.5e18);
+        assertEq(blockNum, 1);
+    }
+
+    function testWithdrawTokensMustApprove() public {
+        mockToken.mint(alice, 10000e18);
+        vm.prank(alice);
+        // mockToken.approve(address(escrow), 1e18);
+
+        vm.roll(1);
+        vm.expectRevert();
+        vm.prank(alice);
+        escrow.depositTokens(address(mockToken), bob, 1e18);
+    }
+}
